5ca90b8d6f64f4b64d8828827cee272f76033ae1
Arthur Neves <arthurnvsm@gmail.com>
2021-12-07 14:52:28 -0300

    NewTaskItemView created

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -11,8 +11,6 @@ import CoreData
 struct ContentView: View {
   @State var task = ""
   
-  private var isButtonDisabled: Bool { task.isEmpty }
-  
   // MARK: - FETCHING DATA
   @Environment(\.managedObjectContext) private var viewContext
   
@@ -25,30 +23,6 @@ struct ContentView: View {
     NavigationView {
       ZStack {
         VStack {
-          VStack(spacing: 16) {
-            TextField("New Task", text: $task)
-              .padding()
-              .background(
-                Color(UIColor.systemGray6)
-              )
-              .cornerRadius(10)
-            
-            Button(action: {
-              addItem()
-            }) {
-              Spacer()
-              Text("SAVE")
-              Spacer()
-            } //: Button
-            .disabled(isButtonDisabled)
-            .padding()
-            .font(.headline)
-            .foregroundColor(.white)
-            .background(isButtonDisabled ? Color.gray : Color.pink)
-            .cornerRadius(10)
-          } //: VStack
-          .padding()
-          
           List {
             ForEach(items) { item in
               VStack(alignment: .leading) {
@@ -86,26 +60,6 @@ struct ContentView: View {
       )
     } //: NavigationView
     .navigationViewStyle(StackNavigationViewStyle())
-  }
-  
-  private func addItem() {
-    withAnimation {
-      let newItem = Item(context: viewContext)
-      newItem.timestamp = Date()
-      newItem.task = task
-      newItem.completion = false
-      newItem.id = UUID()
-      
-      do {
-        try viewContext.save()
-      } catch {
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
-      
-      task = ""
-      hideKeyboard() //UIKit extension
-    }
   }
   
   private func deleteItems(offsets: IndexSet) {
---
+++ Devote/View/NewTaskItemView.swift
@@ -0,0 +1,81 @@
+//
+//  NewTaskItemView.swift
+//  Devote
+//
+//  Created by Arthur Neves on 07/12/21.
+//
+
+import SwiftUI
+
+struct NewTaskItemView: View {
+  @Environment(\.managedObjectContext) private var viewContext
+  @State private var task = ""
+  
+  private var isButtonDisabled: Bool { task.isEmpty }
+  
+  // MARK: - FUNCTION
+  private func addItem() {
+    withAnimation {
+      let newItem = Item(context: viewContext)
+      newItem.timestamp = Date()
+      newItem.task = task
+      newItem.completion = false
+      newItem.id = UUID()
+      
+      do {
+        try viewContext.save()
+      } catch {
+        let nsError = error as NSError
+        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+      }
+      
+      task = ""
+      hideKeyboard() //UIKit extension
+    }
+  }
+  
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      VStack(spacing: 16) {
+        TextField("New Task", text: $task)
+          .padding()
+          .background(
+            Color(UIColor.systemGray6)
+          )
+          .cornerRadius(10)
+        
+        Button(action: {
+          addItem()
+        }) {
+          Spacer()
+          Text("SAVE")
+          Spacer()
+        } //: Button
+        .disabled(isButtonDisabled)
+        .padding()
+        .font(.headline)
+        .foregroundColor(.white)
+        .background(isButtonDisabled ? Color.gray : Color.pink)
+        .cornerRadius(10)
+      } //: VStack
+      .padding(.horizontal)
+      .padding(.vertical, 20)
+      .background(
+        Color.white
+      )
+      .cornerRadius(16)
+      .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
+      .frame(maxWidth: 640)
+    } //: Vstack
+    .padding()
+  }
+}
+
+struct NewTaskItemView_Previews: PreviewProvider {
+  static var previews: some View {
+    NewTaskItemView()
+      .background(Color.gray.edgesIgnoringSafeArea(.all))
+  }
+}
